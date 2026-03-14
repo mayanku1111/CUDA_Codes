63d83d04e7d8504a83d08791954ce08e42a6e03e
mayanku1111 <138918191+mayanku1111@users.noreply.github.com>
2026-03-14 16:23:23 +0530

    minor changes

--- Learning_CUDA/convolution_tiling.cu
+++ Learning_CUDA/convolution_tiling.cu
@@ -1,2 +1,2 @@
 
-// Store zero to shared memory tile for out of bounds input elements
+// Store zero to shared memory tile for out of bounds input elements for example in the cases of block(0,0) and others
